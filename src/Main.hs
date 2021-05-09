eeeef499a65228feda0abbbdc953f62acf888471
Samuel Gélineau <gelisam+github@gmail.com>
2021-05-09 16:17:34 -0400

    display the first frame of the gif

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,43 @@
 module Main where
 
+import Codec.Picture.Gif
 import Graphics.Gloss
+import Graphics.Gloss.Juicy
 import System.Environment
+import qualified Data.ByteString as ByteString
 
 
 pinkBg :: Color
 pinkBg = makeColorI 0xfe 0xe5 0xfc 0xff
+
+bg :: Picture
+bg = color pinkBg
+   $ rectangleSolid 640 339
+
+app :: [Picture] -> IO ()
+app frames = do
+  display
+    (InWindow "Nice Window" (678, 427) (10, 10))
+    white
+    (bg <> frame 0)
+  where
+    frame :: Int -> Picture
+    frame i = frames !! i
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [gifPath] -> do
-      display
-        (InWindow "Nice Window" (678, 427) (10, 10))
-        white
-        ( color pinkBg
-        $ rectangleSolid 640 339
-        )
+      gifBytestring <- ByteString.readFile gifPath
+      case decodeGifImages gifBytestring of
+        Left err -> do
+          error err
+        Right frameDynamicImages -> do
+          case traverse fromDynamicImage frameDynamicImages of
+            Nothing -> do
+              error "gloss does not support this image format"
+            Just framePictures -> do
+              app framePictures
     _ -> do
       putStrLn "usage: gif-browser GIF_FILE"
