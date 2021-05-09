805b8959f2ed8421408e8173208507d041bb6cf3
Samuel Gélineau <gelisam+github@gmail.com>
2021-05-09 16:02:56 -0400

    parse command-line arguments

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Graphics.Gloss
+import System.Environment
 
 
 pinkBg :: Color
@@ -8,9 +9,14 @@ pinkBg = makeColorI 0xfe 0xe5 0xfc 0xff
 
 main :: IO ()
 main = do
-  display
-    (InWindow "Nice Window" (678, 427) (10, 10))
-    white
-    ( color pinkBg
-    $ rectangleSolid 640 339
-    )
+  args <- getArgs
+  case args of
+    [gifPath] -> do
+      display
+        (InWindow "Nice Window" (678, 427) (10, 10))
+        white
+        ( color pinkBg
+        $ rectangleSolid 640 339
+        )
+    _ -> do
+      putStrLn "usage: gif-browser GIF_FILE"
